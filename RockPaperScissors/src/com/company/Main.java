b0bca8d1d2922a500478ac5cd970e0ce81e84a3c
nakamura.g <nakamura0422gou@gmail.com>
2018-01-13 19:54:17 +0900

    first commit

---
+++ RockPaperScissors/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * メインクラス
+ *
+ * クラス名は大文字始まりだよ
+ * 2単語以上ある場合は "HogehogeFoo" みたいになるよ
+ */
+public class Main {
+
+    /**
+     * メインメソッド
+     *
+     * メソッド名は動詞で小文字にしようね
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ RockPaperScissors/src/com/company/Player.java
@@ -0,0 +1,24 @@
+package com.company;
+
+/**
+ * プレイヤークラス
+ *
+ * プレイヤーが何を持ってて何をするのかを書くよ
+ */
+public class Player {
+
+    /**
+     * メンバ定数
+     * 頭に"m"つけなくていいと思うよ(賛否両論)
+     * 定数の命名規則は全部大文字で"_"つながりだよ
+     *
+     * グー チー パー はintで持ってもStringで持ってもいいよ
+     * Stringの方がわかりやすいかもしれないね
+     */
+    final String ROOK = "グー";
+    final String PAPER = "パー";
+    final String SCISSORS = "チー";
+
+
+
+}
